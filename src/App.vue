c3972d772b63f0b8e789a439de7460cc734c2f88
TIMER_err <zqq2333@outlook.com>
2024-09-16 02:55:32 +0800

    live2d

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,18 @@
   <header>
   </header>
 
-
-
   <main>
 
     <div class="card w-96 bg-base-100 shadow-xl image-full" id="pagebox">
-      <figure><img src="./assets/YolBilogo.png" alt="YolBi" /></figure>
+      <figure><img src="./assets/YolBilogo.png" alt="YolBi"/></figure>
       <div class="card-body">
         <h2 class="card-title">It's YolBi</h2>
         <p>But the website is still making</p>
         <div class="card-actions justify-end">
-          <button @click="handleClickButton" class="btn btn-primary">Join Now</button>
+          <button @click="handleGithub" class="btn btn-primary">GitHub</button>
+        </div>
+        <div class="card-actions justify-end">
+          <button @click="handleJoinNow" class="btn btn-primary">QQ Group</button>
         </div>
       </div>
     </div>
@@ -25,12 +26,15 @@
 
 export default {
   methods: {
-    handleClickButton() {
+    handleJoinNow() {
       console.log('clicked');
-      window.open("http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=n5HdIsm7at6tD7sSLrZv_lNDZp63M69a&authKey=DOC%2FqqaDW6Wd4ffIj2a%2F5KBNqPc6NQFnWSv1xQZK7lfglQ76LrbE8GGtELhdkILi&noverify=0&group_code=699481681");
+      window.open("https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=n5HdIsm7at6tD7sSLrZv_lNDZp63M69a&authKey=DOC%2FqqaDW6Wd4ffIj2a%2F5KBNqPc6NQFnWSv1xQZK7lfglQ76LrbE8GGtELhdkILi&noverify=0&group_code=699481681");
+    },
+    handleGithub() {
+      console.log('clicked');
+      window.open("https://github.com/yapeteam");
     },
   }
-
 }
 
 </script>
